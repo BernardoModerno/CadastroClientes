f084822c232590a576cb034eb5ec4434672679d2
BernardoModerno <bernardo.moderno@gmail.com>
2023-01-11 12:44:37 -0300

    frontend

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import SignIn from './pages/Auth/SignIn';
-import SignUp from './pages/Auth/SignUp';
-import Home from './pages/Dashboard/Home';
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import Home from "./pages/Dashboard/Home";
+import { ChakraProvider } from "@chakra-ui/react";
+
+interface PrivateRouteProps {
+  children: JSX.Element;
+  redirectTo: string;
+}
+
+const PrivateRoute = ({
+  children,
+  redirectTo,
+}: PrivateRouteProps): JSX.Element => {
+  const isAuthenticated = localStorage.getItem("token") !== null;
+  return isAuthenticated ? children : <Navigate to={redirectTo} />;
+};
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/home' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/home"
+            element={
+              <PrivateRoute redirectTo="/">
+                <Home />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
